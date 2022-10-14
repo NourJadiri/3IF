d403671e40ba6705d77ee8a2f1e38067e00d1c37
Nour <mohamed-nour.eljadiri@insa-lyon.fr>
2022-10-14 20:07:30 +0200

    Tas binaire mais pour l'instant c'est faux

--- TD4/TasBinaire.c
+++ TD4/TasBinaire.c
@@ -5,7 +5,7 @@
 typedef struct {
     int allocated; /* current allcoation of array */
     int filled;    /* number of items present in the binheap */
-    int *array;    /* array of values */
+    int *array;/* array of values */
 } BinaryHeap;
 
 /* Init allocates the structure BinaryHeap and
@@ -30,6 +30,10 @@ int ExtractMax(BinaryHeap * heap, int * val);
 /* Destroy frees the structure and the array */
 void Destroy(BinaryHeap * heap);
 
+/*Cette fonction sera super utile hihi*/
+void swap(int *xp, int *yp);
+
+int TestChildren(BinaryHeap * heap, int i);
 
 int main(void)
 {
@@ -58,23 +62,81 @@ int main(void)
 
 BinaryHeap * Init(int size)
 {
-    BinaryHeap * heap;
-    /* put your init code here */
-    return heap;
+    BinaryHeap *heap1;
+    heap1 = (BinaryHeap*)malloc(sizeof(BinaryHeap));
+    heap1 -> allocated = size;
+    heap1->filled = 0;
+    heap1->array = (int*)malloc(sizeof(int) * size);
+
+    return heap1;
 }
 
+// Voir si un tas binaire est toujours ordonné
 void InsertValue(BinaryHeap * heap, int value)
 {
-    /* put your insert code here */
+    if (heap->filled == heap->allocated){
+        heap->allocated *= 2;
+        heap->array = (int*) realloc(heap->array,heap->allocated);
+    }
+    heap->array[heap->filled++] = value;
+
+    int i = heap->filled;
+
+    while(heap->array[(i-1)/2] < heap->array[i]){
+        swap(&(heap->array[i]),&(heap->array[(i-1)/2]));
+        i = (i-1)/2;
+    }
+
+    for(int i = 0 ; i < sizeof(heap->array)/sizeof(int) ; i++){
+        printf("%d ",heap->array[i]);
+    }
+
+}
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
 
 int ExtractMax(BinaryHeap * heap, int *res)
 {
-    /* put your extraction code here */
-    return 0;
+    if(!heap->filled) return -1;
+
+    int max = heap->array[0];
+
+    swap(&(heap->array[0]),&(heap->array[heap->filled]));
+    heap->filled--;
+    int i = 0;
+
+    while(TestChildren(heap,i)){
+        if(TestChildren(heap,i) == 1) {
+            swap((&heap->array[i]), &(heap->array[2*i+1]));
+            i = 2*i+1;
+        }
+        else {
+            swap((&heap->array[i]), &(heap->array[2*i+2]));
+            i = 2*i + 2 ;
+        }
+    }
+
+
+    return max;
 }
+
+int TestChildren(BinaryHeap * heap, int i) {
+    if (2*i+1 >= heap->filled) return 0; // pas d'enfants
+    if (2*i+2 >= heap->filled){ // seulement enfant a gauche
+        if(heap->array[i] < heap->array[2*i+1]) return 1;
+    }
+    return 1 + heap->array[2*i+1] < heap->array[2*i+2];
+}
+
 
 void Destroy(BinaryHeap * heap)
 {
-    /* put your destruction code here */
+    free(heap->array);
+    free(heap);
+
 }

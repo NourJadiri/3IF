4f0e36285f8947706cd3e57558161beb46544ca3
VincentVoigtlaender <80749062+VincentVoigtlaender@users.noreply.github.com>
2022-11-22 15:59:40 +0100

    Create Trip.cpp

---
+++ C++/TP2/Trip.cpp
@@ -0,0 +1,63 @@
+/*************************************************************************
+                           Trip  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Trip> (fichier Trip.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Trip.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type Trip::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+Trip & Trip::operator = ( const Trip & unTrip )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Trip::Trip ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Trip>" << endl;
+#endif
+} //----- Fin de Trip
+
+
+Trip::~Trip ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Trip>" << endl;
+#endif
+} //----- Fin de ~Trip
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées

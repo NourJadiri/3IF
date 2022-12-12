2da3b1898883e0880044ae84166ca4e4a4cdb876
VincentVoigtlaender <80749062+VincentVoigtlaender@users.noreply.github.com>
2022-12-12 17:50:49 +0100

    Update Pb-1-Question-7.sql

--- SGBD/TP1/DS/Pb-1-Question-7.sql
+++ SGBD/TP1/DS/Pb-1-Question-7.sql
@@ -7,10 +7,14 @@
 -- Probleme 1 - Question 7 : Pour chaque utilisateur, donnez le code permettant de lister ses posts les mieux notÃ©s.
 
 with best_note as
-  (select utilisateurId, max(note) as max_note
-  from utilisateur natural join (post natural join note)
-  group by utilisateurId)
+  (select u.UTILISATEURID, max(n.note) as max_note
+  from utilisateur u, post p, note n 
+  where u.UTILISATEURID = p.UTILISATEURID and p.postId = n.postId
+  group by u.UTILISATEURID)
   
-select nom, postId
-from utilisateur natural join (post natural join note) natural join best_note
-where note = max_note;
+select u.nom, p.postId
+  from utilisateur u, post p, note n, best_note
+  where u.UTILISATEURID = p.UTILISATEURID 
+    and p.postId = n.postId
+    and best_note.UTILISATEURID = u.UTILISATEURID
+    and n.note = best_note.max_note;

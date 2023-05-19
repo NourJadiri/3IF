d80c462eeedb7614329934bbe1951da61c6d12d5
Nour <mohamed-nour.eljadiri@insa-lyon.fr>
2023-05-19 18:23:01 +0200

    Question bonus done

--- S2/Proba/files_d_attente.R
+++ S2/Proba/files_d_attente.R
@@ -1,26 +1,9 @@
 FileMM1 <- function(lambda, mu, D){
 
-  # On génère la premiere valeur d'arrivée qui suit une loi exponentielle de parametre lambda
-  arrivee1 <- rexp(1,lambda)
-  # fin represente la valeur maximale d'arrivée d'un client
-  fin <- arrivee1 + D
+  arrivees <- generate_arrivees(lambda , D)
 
-  #Vecteur contenant les temps d'arrivé des clients
-  arrivees <- c()
-  arrivees <- c(arrivees,arrivee1)
-
-  it <- 2
-  # tant que l'heure d'arrivée du dernier client est inférieur à fin on rajoute une valeur d'heure d'arrivée qui correspond à l'heure d'arrivée du client précédent + une valeur générée par la loi exponentionnel
-  while(arrivees[length(arrivees)] < fin )
-  {
-    arrivees <- c(arrivees, arrivees[it-1] + rexp(1,lambda))
-    it <- it + 1
-  }
-
-  arrivees <- arrivees[-length(arrivees)]
-
-  #de la meme maniere, on génère l'heure de départ du premier client
-  depart1 <- arrivee1 + rexp(1,mu)
+  # de la meme maniere, on génère l'heure de départ du premier client
+  depart1 <- arrivees[1] + rexp(1,mu)
   departs <- c()
   departs<- c(departs, depart1)
 
@@ -30,7 +13,7 @@ FileMM1 <- function(lambda, mu, D){
     departs <- c(departs , max(arrivees[i],departs[i-1]) + rexp(1,mu))
 
     # Si le service ferme, on garde les gens prisonniers
-    if(departs[i] > arrivee1 + D){
+    if(departs[i] > arrivees[1] + D){
       departs <- departs[-length(departs)]
       break
     }
@@ -114,4 +97,54 @@ collect_results <- function(means, average_wait_times){
   results$Ecart_Relatif <- abs((results$En - results$LittleFormula)/results$En)
 
   return (results)
+}
+
+FileMM2 <- function(lambda, mu, D){
+
+  arrivees <- generate_arrivees(lambda, D)
+
+  departs <- c(arrivees[1] + rexp(1,mu) , arrivees[2] + rexp(1 ,mu))
+
+  for(i in 3:length(arrivees)){
+    # Plusieurs cas sont possibles:
+      # Si le client arrive et que au moins un des serveurs est libre, alors son traitement se fait automatiquement ( arrivees[i] + rexp(1,mu) )
+      # Si les deux serveurs sont occupés, le client attend le premier serveur qui se libère ( min(departs[i-1],departs[i-2]) + rexp(1,mu) )
+    # On prend le max de ces deux possibilités
+    departs[i] <- max(arrivees[i],min(departs[i-1],departs[i-2])) + rexp(1,mu)
+
+    if(departs[i] > arrivees[1] + D){
+      departs <- departs[-length(departs)]
+      break
+    }
+  }
+
+  return (list(arr = arrivees,dep = departs))
+}
+
+generateFileMM2 <- function(duration){
+  ma_liste1 <- FileMM1(10,20,duration)
+  ma_liste2 <- FileMM1(14,20,duration)
+  ma_liste3 <- FileMM1(20,20,duration)
+  ma_liste4 <- FileMM1(30,20,duration)
+
+  return(list(test1 = ma_liste1, test2 = ma_liste2, test3 = ma_liste3, test4 = ma_liste4))
+}
+
+
+generate_arrivees <- function(lambda, D){
+  arrivees <- rexp(1, lambda)
+
+  fin <- arrivees[1] + D
+
+  it <- 2
+  # tant que l'heure d'arrivée du dernier client est inférieur à fin on rajoute une valeur d'heure d'arrivée qui correspond à l'heure d'arrivée du client précédent + une valeur générée par la loi exponentionnel
+  while(arrivees[length(arrivees)] < fin )
+  {
+    arrivees <- c(arrivees, arrivees[it-1] + rexp(1,lambda))
+    it <- it + 1
+  }
+
+  arrivees <- arrivees[-length(arrivees)]
+
+  return (arrivees)
 }
